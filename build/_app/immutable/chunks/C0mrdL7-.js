f06b266a7ec7803b62f1dbb4e3282c7d359789ab
CousCous08 <marcusnglee@gmail.com>
2025-06-19 17:47:43 -0400

    change base

--- build/_app/immutable/chunks/C0mrdL7-.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1km8g5t)==null?void 0:s.base)??"/marcusnglee.github.io";var t;const a=((t=globalThis.__sveltekit_1km8g5t)==null?void 0:t.assets)??e;export{a,e as b};
--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base:
-        process.env.NODE_ENV === "production" ? "/marcusnglee.github.io" : "",
+      base: "",
     },
   },
 };
