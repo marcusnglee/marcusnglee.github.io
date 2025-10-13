e43f4332e6a87a8ff87d5fbe83430cd8383b5a2e
CousCous08 <marcusnglee@gmail.com>
2025-10-13 15:07:03 -0400

    bio update

--- src/lib/components/Bio.svelte
+++ src/lib/components/Bio.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div class='bio'>
+    <div class="paragraph">
     <p>
-        <span class="name"><strong>Marcus Lee</strong></span> is a student pursuing his BA in Computer Science at Williams College. 
-        His work primarily encompasses distributed systems infrastructure and frontend design, with additional curiosity
-        in the intersection of (computing ∩ art ∩ culture). Between kubernetes and svelte, 
-        he contemplates over his lightly-brewed cup of tea.
+        <span class="name"><strong>Marcus Lee</strong></span> is a student pursuing his BA in Computer Science at Williams College. His work spans distributed systems, automated cloud infrastructure, and frontend design. Currently, he's exploring thoughtful AI tooling for students and decentralized economies—
+        <a target="_blank" rel="noopener noref" href="https://medium.com/design-bootcamp/we-love-automation-but-hate-ai-what-ux-teaches-us-about-control-and-trust-c2f41c29906b"><u>building software for people that makes them feel empowered and in control.</u></a> Between Kubernetes and Svelte, he'll always welcome another to conversation over a lightly-brewed cup of tea. This is also the only time Marcus will describe himself in the 3rd person.
     </p>
+</div>
     <div class='links'>
         <a target="_blank" rel="noopener noref" href="https://www.linkedin.com/in/marcusnglee">linkedin.com/in/marcusnglee</a>
         <a target="_blank" rel="noopener noref" href="https://www.github.com/marcusnglee">github.com/marcusnglee</a>
@@ -26,6 +26,13 @@
     font-family: Montserrat;
     font-size: medium;
     color: rgb(113, 113, 113);
+}
+.paragraph a{
+    color: rgb(113, 113, 113);
+    transition: color 0.2s ease;
+}
+.paragraph a:hover{
+    color: rgb(59, 51, 51);   
 }
 .links a{
     color: rgb(59, 51, 51);   
